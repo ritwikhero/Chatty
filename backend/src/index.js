60ccc7a35abe29ba79865b97478b6cc7e0c77e1f
Ritwik <ritwikhero@gmail.com>
2025-07-14 00:42:38 +0530

    message routes added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -12,6 +13,8 @@ const PORT = process.env.PORT;
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+
+app.use("/api/auth", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
