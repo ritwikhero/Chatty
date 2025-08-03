ed32a121bfc0f064d83c73af543e02152e03fcdb
Ritwik <ritwikhero@gmail.com>
2025-08-03 17:12:09 +0530

    route changed

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,7 +27,7 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/{*any}", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
